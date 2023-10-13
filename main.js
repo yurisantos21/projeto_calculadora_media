74c1421527f3865958ad23116539756aae0a62d1
Yuri Santos <yuris.santos21@outlook.com>
2023-10-13 12:56:52 -0300

    Add files via upload
    Adicionados arquivos base do projeto.

---
+++ main.js
@@ -0,0 +1,16 @@
+const form = document.getElementById('form-atividade');
+form.addEventListener('submit', function(e){
+	e.preventDefault();
+
+	const inputNomeAtividade = document.getElementById('nome-atividade');
+	const inputNotaAtividade = document.getElementById('nota-atividade');
+
+	let linha = '<tr>';
+	let linha += `<td>${inputNomeAtividade.value}</td>`;
+	let linha += `<td>${inputNotaAtividade.value}</td>`;
+	let linha += `<td>${inputNotaAtividade.value >= 7 ? 'Aprovado' ; 'Reprovado'}</td>`;
+	let linha += '</tr>';
+
+	const corpoTabela = document.querySelector('tbody');
+	corpoTabela.innerHTML = linha;
+})
